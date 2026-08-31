08493d1ab2a1975624eacf0a083f2359ce66b67c
Andrew Roddy <andrewroddy7@gmail.com>
2026-08-31 19:22:59 -0400

    Now does file io

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
 def main():
-    print("HIHI")
+
+    file = open(file="example.txt", mode="r") # Opens the file
+    text = file.read().strip() # Reads file and removes newlines
+    file.close() # Closes the file
+
+    for char in text:
+        print(char + "|")
 
 
 if __name__ == "__main__":
