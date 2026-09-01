205f625558e2fb34e64267991740b168236f358f
Andrew Roddy <andrewroddy7@gmail.com>
2026-08-31 20:00:52 -0400

    Renamed files to fit requirements

---
+++ runner.py
@@ -0,0 +1,21 @@
+import sys
+print(sys.argv)
+
+def main():
+
+    # Checks if the correct number of command line argument exists
+    if len(sys.argv) != 2:
+        print(f"Needs two system arguments.\n1: The program name\n2: The input file\nGiven: {sys.argv}")
+        return
+
+    input_file = sys.argv[1] # Gets input file
+    file = open(file=input_file, mode="r") # Opens the file
+    text = file.read().strip() # Reads file and removes newlines
+    file.close() # Closes the file
+
+    # Iterates each character in the text
+    for char in text:
+        print("|" + char + "|")
+
+if __name__ == "__main__":
+    main()
